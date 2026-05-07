dd22c7c150075eb6c69e91ab30f2fd218ab37fad
Michael Bishop <michael@bishop.net.za>
2026-05-07 17:39:28 +0200

    Add meta route

--- routes/api.php
+++ routes/api.php
@@ -7,11 +7,10 @@ use App\Http\Controllers\Api\TaskQueryController;
 use App\Http\Controllers\ClientAuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/clients/register', [ClientAuthController::class, 'register']);
 Route::post('/auth/token', [ClientAuthController::class, 'token']);
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('tasks/meta', [TaskMetaController::class, 'index']);
+    Route::get('meta/task-statuses', [TaskMetaController::class, 'index']);
     Route::get('tasks/assignee/{email}', [TaskQueryController::class, 'byAssignee']);
     Route::apiResource('tasks', TaskController::class);
     Route::apiResource('projects', ProjectController::class);
