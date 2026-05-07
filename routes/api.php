7e90e9940e5233b566b9ea2f95c51a3440d57cb2
Michael Bishop <michael@bishop.net.za>
2026-05-07 13:19:03 +0200

    Fix token route

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\ClientAuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/clients/register', [ClientAuthController::class, 'register']);
-Route::post('/clients/token', [ClientAuthController::class, 'token']);
+Route::post('/auth/token', [ClientAuthController::class, 'token']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('tasks/meta', [TaskMetaController::class, 'index']);
