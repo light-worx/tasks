dcfa7c317a9dc413583b176caafe2d715c003c47
Michael Bishop <michael@bishop.net.za>
2026-05-08 11:32:34 +0200

    Add task count

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -7,21 +7,33 @@ use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller {
-
     public function index(Request $request)
     {
         $client = $request->user();
+
         return Project::query()
-            ->where('organisation_id', $client->organisation_id)
+            ->where(
+                'organisation_id',
+                $client->organisation_id
+            )
             ->where(function ($query) use ($request) {
                 $query->where('is_private', false);
                 if ($request->filled('owner_email')) {
                     $query->orWhere(function ($private) use ($request) {
                         $private
-                            ->where('is_private', true)->where('owner_email',$request->owner_email);
+                            ->where('is_private', true)
+                            ->where(
+                                'owner_email',
+                                $request->owner_email
+                            );
                     });
                 }
-            })->orderBy('name')->get();
+            })
+            ->withCount([
+                'tasks as task_count',
+            ])
+            ->orderBy('name')
+            ->get();
     }
 
     public function store(Request $request)
@@ -32,10 +44,13 @@ class ProjectController extends Controller {
             'is_private' => 'boolean',
             'owner_email' => 'nullable|email',
         ]);
+
         return Project::create([
             ...$validated,
+
             'organisation_id' =>
                 $request->user()->organisation_id,
+
             'created_by_client_id' =>
                 $request->user()->id,
         ]);
