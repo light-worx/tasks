4c986be4f53a5a4dbe22f19c326351bc77623195
Michael Bishop <michael@bishop.net.za>
2026-07-24 08:22:08 +0200

    Add api route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\ContextController;
 use App\Http\Controllers\Api\ProjectController;
 use App\Http\Controllers\Api\TaskController;
 use App\Http\Controllers\Api\TaskMetaController;
@@ -12,6 +13,7 @@ Route::post('/auth/token', [ClientAuthController::class, 'token']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('meta/task-statuses', [TaskMetaController::class, 'index']);
     Route::get('tasks/assignee/{email}', [TaskQueryController::class, 'byAssignee']);
+    Route::get('contexts', [ContextController::class, 'index']);
     Route::apiResource('tasks', TaskController::class);
     Route::apiResource('projects', ProjectController::class);
 });
